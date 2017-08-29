3cb988d38e0360cc8b37a95c717e55bcd051baa1
5620l <mahdihekmat23@gmail.com>
2017-08-29 04:31:35 -0700

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,118 +1,188 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# Launch created by @Jarriz, @Josepdal and @iicc1
+tgcli_version=1222
+TMUX_SESSION=DBTeamV2
+luarocks_version=2.4.2
 
-THIS_DIR=$(cd $(dirname $0); pwd)
-cd $THIS_DIR
+lualibs=(
+'luasec'
+'luarepl'
+'lbase64 20120807-3'
+'luafilesystem'
+'lub'
+'luaexpat'
+'redis-lua'
+'lua-cjson'
+'fakeredis'
+'xml'
+'feedparser'
+'serpent'
+)
 
-update() {
-  git pull
-  git submodule update --init --recursive
-  install_rocks
+today=`date +%F`
+
+get_sub() {
+    local flag=false c count cr=$'\r' nl=$'\n'
+    while IFS='' read -d '' -rn 1 c; do
+        if $flag; then
+            printf '%c' "$c"
+        else
+            if [[ $c != $cr && $c != $nl ]]; then
+                count=0
+            else
+                ((count++))
+                if ((count > 1)); then
+                    flag=true
+                fi
+            fi
+        fi
+    done
 }
 
-# Will install luarocks on THIS_DIR/.luarocks
-install_luarocks() {
-  git clone https://github.com/keplerproject/luarocks.git
-  cd luarocks
-  git checkout tags/v2.2.1 # Current stable
-
-  PREFIX="$THIS_DIR/.luarocks"
-
-  ./configure --prefix=$PREFIX --sysconfdir=$PREFIX/luarocks --force-config
-
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  make build && make install
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting.";exit $RET;
-  fi
-
-  cd ..
-  rm -rf luarocks
+get_firstname_by_username() {
+	user=`./bin/telegram-cli --disable-link-preview -C -R -D -e \
+	'resolve_username '${1}'' 2>/dev/null`
+    space_index=`echo ${user} | tr -cd ' '  | wc -c`
+	msg=null
+	if [[ ! ${user} ]] || [[ ${user} =~ 400 ]]; then
+		echo $msg
+	else
+		msg=`echo ${user} | cut -d ' ' -f${space_index}- | cut -d ' ' -f1`
+		echo "${msg}"
+	fi
 }
 
-install_rocks() {
-  ./.luarocks/bin/luarocks install luasocket
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install oauth
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install redis-lua
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install lua-cjson
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install fakeredis
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install xml
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install feedparser
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install serpent
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
+make_progress() {
+exe=`lua <<-EOF
+    print(tonumber($1)/tonumber($2)*100)
+EOF
+`
+    echo ${exe:0:4}
 }
 
-install() {
-  git pull
-  git submodule update --init --recursive
-  patch -i "patches/disable-python-and-libjansson.patch" -p 0 --batch --forward
-  RET=$?;
 
-  cd tg
-  if [ $RET -ne 0 ]; then
-    autoconf -i
-  fi
-  ./configure && make
 
-  RET=$?; if [ $RET -ne 0 ]; then
-    echo "Error. Exiting."; exit $RET;
-  fi
-  cd ..
-  install_luarocks
-  install_rocks
+function download_libs_lua() {
+    if [[ ! -d "logs" ]]; then mkdir logs; fi
+    if [[ -f "logs/logluarocks_${today}.txt" ]]; then rm logs/logluarocks_${today}.txt; fi
+    local i
+    for ((i=0;i<${#lualibs[@]};i++)); do
+        printf "\r\33[2K"
+        printf "\rDBTeam: wait... [`make_progress $(($i+1)) ${#lualibs[@]}`%%] [$(($i+1))/${#lualibs[@]}] ${lualibs[$i]}"
+        ./.luarocks/bin/luarocks install ${lualibs[$i]} &>> logs/logluarocks_${today}.txt
+    done
+    sleep 0.2
+    printf "\nLogfile created: $PWD/logs/logluarocks_${today}.txt\nDone\n"
+    rm -rf luarocks-2.2.2*
 }
 
-if [ "$1" = "install" ]; then
-  install
-elif [ "$1" = "update" ]; then
-  update
-else
-  if [ ! -f ./tg/telegram.h ]; then
-    echo "tg not found"
-    echo "Run $0 install"
-    exit 1
-  fi
+function update() {
+    git checkout launch.sh plugins/ lang/ bot/ libs/
+    git pull
+    echo chmod +x launch.sh | /bin/bash
+}
 
-  if [ ! -f ./tg/bin/telegram-cli ]; then
-    echo "tg binary not found"
-    echo "Run $0 install"
-    exit 1
-  fi
-  while true; do
-   rm -r ../.telegram-cli/state
-   ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/bot.lua -l 1 -E $@
-   sleep 3
-  done
-fi
+function configure() {
+    if [[ -f "/usr/bin/lua5.3" ]] || [[ -f "/usr/bin/lua5.1" ]] || [[ -f "/usr/local/bin/lua5.3" ]]; then
+        echo -e "\033[0;31mError\033[0m":\
+        "DBTeam ins't working with lua5.3 and others versions, the packages must be removed,"\
+        "please remove them, reinstall lua5.2 and run launch.sh again."
+        exit 1
+    fi
+    dir=$PWD
+    wget http://luarocks.org/releases/luarocks-${luarocks_version}.tar.gz &>/dev/null
+    tar zxpf luarocks-${luarocks_version}.tar.gz &>/dev/null
+    cd luarocks-${luarocks_version}
+    if [[ ${1} == "--no-null" ]]; then
+        ./configure --prefix=$dir/.luarocks --sysconfdir=$dir/.luarocks/luarocks --force-config
+        make bootstrap
+    else
+        ./configure --prefix=$dir/.luarocks --sysconfdir=$dir/.luarocks/luarocks --force-config &>/dev/null
+        make bootstrap &>/dev/null
+    fi
+    cd ..; rm -rf luarocks*
+    if [[ ${1} != "--no-download" ]]; then
+        download_libs_lua
+        wget --progress=bar:force https://valtman.name/files/telegram-cli-${tgcli_version} 2>&1 | get_sub
+        if [ ! -d "bin" ]; then mkdir bin; fi
+        mv telegram-cli-${tgcli_version} ./bin/telegram-cli; chmod +x ./bin/telegram-cli
+    fi
+    for ((i=0;i<101;i++)); do
+        printf "\rConfiguring... [%i%%]" $i
+        sleep 0.007
+    done
+    printf "\nDone\n"
+}
+
+function start_bot() {
+    ./bin/telegram-cli -s ./bot/bot.lua $@
+}
+
+function show_logo_slowly() {
+    seconds=0.009
+    logo=(
+    " ____  ____ _____"
+    "|    \|  _ )_   _|___ ____   __  __"
+    "| |_  )  _ \ | |/ .__|  _ \_|  \/  |"
+    "|____/|____/ |_|\____/\_____|_/\/\_|  v2"
+    "by @Josepdal @iicc1 and @Jarriz"
+    )
+    printf "\033[38;5;208m\t"
+    local i x
+    for i in ${!logo[@]}; do
+        for ((x=0;x<${#logo[$i]};x++)); do
+            printf "${logo[$i]:$x:1}"
+            sleep $seconds
+        done
+        printf "\n\t"
+    done
+    printf "\n"
+}
+
+function show_logo() {
+    #Adding some color. By @iicc1 :D
+    echo -e "\033[38;5;208m"
+    echo -e "\t ____  ____ _____"
+    echo -e "\t|    \|  _ )_   _|___ ____   __  __"
+    echo -e "\t| |_  )  _ \ | |/ .__|  _ \_|  \/  |"
+    echo -e "\t|____/|____/ |_|\____/\_____|_/\/\_|  v2"
+    echo -e "\n\e[36m"
+}
+
+case $1 in
+    install)
+    	show_logo_slowly
+    	configure ${2}
+    	exit ;;
+    update)
+    	show_logo
+    	update
+    	exit ;;
+    tmux)
+    	if [ ! -f "/usr/bin/tmux" ]; then echo "Please install tmux"; exit; fi
+    	error=`tmux new-session -s $TMUX_SESSION -d "./bin/telegram-cli -s ./bot/bot.lua" 2>/dev/stdout`
+    	if [[ ! $error ]]; then echo "New session tmux: ${TMUX_SESSION}"; else echo "Error running tmux."; fi
+    	exit ;;
+    attach)
+    	if [ ! -f "/usr/bin/tmux" ]; then echo "Please install tmux"; exit; fi
+		tmux attach-session -t $TMUX_SESSION
+		exit ;;
+    kill)
+    	if [ ! -f "/usr/bin/tmux" ]; then echo "Please install tmux"; exit; fi
+    	tmux kill-session -t $TMUX_SESSION
+    	exit ;;
+    whois)
+        name=`get_firstname_by_username "${2}"`
+        if [[ $name == null ]]; then
+        	echo "Error resolving the user."
+        	exit 1
+        else
+        	echo "${name}"
+        	exit 0
+        fi ;;
+esac
+
+
+show_logo
+start_bot $@
+exit 0
